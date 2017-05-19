b2cc0468d358f2e697ff1b1e21bdedb9736ad3ae
Joaquín Suárez Bodean <jsuarezb@outlook.com>
2017-05-19 02:55:11 -0300

    Adds nyse symbols scrapper

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import collections
 import csv
-import re
 import datetime
+import re
 import requests
 import sys
+
+from functools import reduce
 
 GOOGLE_URI = 'https://www.google.com/finance/getprices?i=60&p=100d&f=d,o,h,l,c,v&df=cpct&q={symbol}'
 
@@ -72,7 +74,7 @@ class GoogleParser:
 FILENAME_FORMAT = 'google-{}.csv'
 HEADER_ROW = ['date', 'open', 'close', 'low', 'high', 'volume']
 
-def main(args):
+def scrap_google():
     symbol = input('Enter the symbol: ')
     data = {'symbol': symbol}
     uri = GOOGLE_URI.format(**data)
@@ -87,6 +89,78 @@ def main(args):
 
     print('Done.')
 
+class NyseScrapper:
+
+    SYMBOLS_PER_PAGE = 10
+
+    SYMBOLS_URI = ('https://www.nyse.com/search?site=idc_instruments'
+                   '&client=nyse_frontend_html&proxystylesheet=ice_frontend_json'
+                   '&requiredfields=INSTRUMENT_TYPE:EQUITY.NORMALIZED_TICKER:{letter}*'
+                   '&getfields=*&num={symbols_per_page}&filter=0&sort=meta:NORMALIZED_TICKER:A'
+                   '&start={start}&wc=1000')
+
+    LETTERS = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+
+    SYMBOL_TICKER = 'SYMBOL_TICKER'
+    INSTRUMENT_NAME = 'INSTRUMENT_NAME'
+
+    def scrap(self):
+        symbols = [self.scrap_letter(letter) for letter in NyseScrapper.LETTERS]
+        return [[k, v] for letter_dicts in symbols for k, v in letter_dicts.items()]
+
+    def scrap_letter(self, letter):
+        print('For letter {0}'.format(letter))
+        symbols = {}
+        current_index = 0
+
+        while True:
+            uri = self.SYMBOLS_URI.format(letter=letter, symbols_per_page=self.SYMBOLS_PER_PAGE,
+                                          start=current_index)
+            response = requests.get(uri)
+            json = response.json()
+            symbols.update(self.parse_symbols(json))
+
+            result_navs = json.get('results_nav')
+            has_next = self.to_int(result_navs.get('have_next'), 0)
+            current_index = self.to_int(result_navs.get('results_end'), 0) + 1
+
+            if has_next != True: # if 2 == True: is True, if 2: is not true
+                break
+
+        return symbols
+
+    def parse_symbols(self, json):
+        symbols = {}
+
+        for result in json.get('results', []):
+            meta_tags = result.get('meta_tags', [])
+            symbol_tickers = [tag.get('value') for tag in meta_tags if tag['name'] == self.SYMBOL_TICKER]
+            instrument_names = [tag.get('value') for tag in meta_tags if tag['name'] == self.INSTRUMENT_NAME]
+
+            if symbol_tickers: symbol_ticker = symbol_tickers[0]
+            if instrument_names: instrument_name = instrument_names[0]
+
+            symbols[symbol_ticker] = instrument_name
+
+        return symbols
+
+    def to_int(self, x, default):
+        return default if x is None or not x else int(x)
+
+
+def scrap_symbols():
+    scrapper = NyseScrapper()
+    data = scrapper.scrap()
+
+    with open('symbols.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+
+    print('Done.')
+
+def main(args):
+    # scrap_google()
+    scrap_symbols()
 
 if __name__ == '__main__':
     main(sys.argv)
